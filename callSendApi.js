f7808c859b7d690fe68ecd163398c5e144d11299
Lukasz Lenart <lukasz.lenart@gmail.com>
2018-01-13 15:20:25 +0100

    Reformats code a bit

--- callSendApi.js
+++ callSendApi.js
@@ -7,18 +7,20 @@ const
 function callSendAPI(sender_psid, response) {
   // Construct the message body
   let request_body = {
-    "recipient": {
-      "id": sender_psid
+    recipient: {
+      id: sender_psid
     },
-    "message": response
+    message: response
   };
 
   // Send the HTTP request to the Messenger Platform
   request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
+    uri: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {
+      access_token: PAGE_ACCESS_TOKEN
+    },
+    method: "POST",
+    json: request_body
   }, (err, res, body) => {
     if (!err) {
       console.log('message sent!')
